d812ce0ea81e9318ef1ce8d25046a53fa49794d2
weizman <weizmangal@gmail.com>
2022-05-30 17:05:20 +0300

    [bugfix] - disable window open API until issue #2 is resolved

--- src/open.js
+++ src/open.js
@@ -1,8 +1,15 @@
 const natives = require('./natives')();
 
+// https://github.com/weizman/glazier/issues/2
+const ISSUE_2_SOLVED = false;
+
 function hookOpen(win, cb) {
     const realOpen = win.open;
     win.open = function() {
+        if (!ISSUE_2_SOLVED) {
+            return null;
+        }
+
         const args = natives['Array'].prototype.slice.call(arguments);
         const opened = realOpen.apply(this, args);
         cb(opened);

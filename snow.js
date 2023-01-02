84b2f331022093a37d0f3ef387b9a3992e21f001
weizman <weizmangal@gmail.com>
2023-01-02 20:35:21 +0200

    imrpove

--- snow.js
+++ snow.js
@@ -173,50 +173,40 @@ function applyHookByString(str, argument, asHtml) {
   return hook + str;
 }
 
-function hookOnLoadAttributes(frames) {
-  for (let i = 0; i < frames.length; i++) {
-    const frame = frames[i];
-    let onload = getAttribute(frame, 'onload');
+function hookOnLoadAttributes(frame) {
+  let onload = getAttribute(frame, 'onload');
 
-    if (onload) {
-      onload = applyHookByString(onload, 'top.SNOW_FRAME_TO_WINDOW(this)', false);
-      setAttribute(frame, 'onload', onload);
-    }
+  if (onload) {
+    onload = applyHookByString(onload, 'top.SNOW_FRAME_TO_WINDOW(this)', false);
+    setAttribute(frame, 'onload', onload);
   }
 }
 
-function hookJavaScriptURI(frames) {
-  for (let i = 0; i < frames.length; i++) {
-    const frame = frames[i];
-    let src = getAttribute(frame, 'src') || '';
-    const [scheme, js] = split(src, ':');
+function hookJavaScriptURI(frame) {
+  let src = getAttribute(frame, 'src') || '';
+  const [scheme, js] = split(src, ':');
 
-    if (stringToLowerCase(scheme) === 'javascript') {
-      src = 'javascript:' + applyHookByString(js, 'window', false);
-      setAttribute(frame, 'src', src);
-    }
+  if (stringToLowerCase(scheme) === 'javascript') {
+    src = 'javascript:' + applyHookByString(js, 'window', false);
+    setAttribute(frame, 'src', src);
   }
 }
 
-function hookSrcDoc(frames) {
-  for (let i = 0; i < frames.length; i++) {
-    const frame = frames[i];
-    let srcdoc = getAttribute(frame, 'srcdoc');
+function hookSrcDoc(frame) {
+  let srcdoc = getAttribute(frame, 'srcdoc');
 
-    if (srcdoc) {
-      srcdoc = applyHookByString(srcdoc, 'window', true);
-      const html = new Array(srcdoc);
-      handleHTML(html, true);
-      setAttribute(frame, 'srcdoc', html[0]);
-    }
+  if (srcdoc) {
+    srcdoc = applyHookByString(srcdoc, 'window', true);
+    const html = new Array(srcdoc);
+    handleHTML(html, true);
+    setAttribute(frame, 'srcdoc', html[0]);
   }
 }
 
 function handleHTML(args, callHook) {
   for (let i = 0; i < args.length; i++) {
-    const html = args[i];
     const template = createElement(document, 'template');
-    setInnerHTML(template, html);
+    setInnerHTML(template, args[i]);
     const content = getTemplateContent(template);
 
     if (!getChildElementCount(content)) {
@@ -225,12 +215,14 @@ function handleHTML(args, callHook) {
 
     const frames = getFramesArray(content, false);
 
-    if (frames.length) {
-      hookOnLoadAttributes(frames);
-      hookJavaScriptURI(frames);
-      hookSrcDoc(frames);
-      args[i] = getInnerHTML(template);
+    for (let j = 0; j < frames.length; j++) {
+      const frame = frames[j];
+      hookOnLoadAttributes(frame);
+      hookJavaScriptURI(frame);
+      hookSrcDoc(frame);
     }
+
+    args[i] = getInnerHTML(template);
 
     if (callHook) {
       args[i] = applyHookByString(args[i], 'window', true);
--- src/html.js
+++ src/html.js
@@ -9,58 +9,49 @@ function applyHookByString(str, argument, asHtml) {
     return hook + str;
 }
 
-function hookOnLoadAttributes(frames) {
-    for (let i = 0; i < frames.length; i++) {
-        const frame = frames[i];
-        let onload = getAttribute(frame, 'onload');
-        if (onload) {
-            onload = applyHookByString(onload, 'top.SNOW_FRAME_TO_WINDOW(this)', false);
-            setAttribute(frame, 'onload', onload);
-        }
+function hookOnLoadAttributes(frame) {
+    let onload = getAttribute(frame, 'onload');
+    if (onload) {
+        onload = applyHookByString(onload, 'top.SNOW_FRAME_TO_WINDOW(this)', false);
+        setAttribute(frame, 'onload', onload);
     }
 }
 
-function hookJavaScriptURI(frames) {
-    for (let i = 0; i < frames.length; i++) {
-        const frame = frames[i];
-        let src = getAttribute(frame, 'src') || '';
-        const [scheme, js] = split(src, ':');
-        if (stringToLowerCase(scheme) === 'javascript') {
-            src = 'javascript:' + applyHookByString(js, 'window', false);
-            setAttribute(frame, 'src', src);
-        }
+function hookJavaScriptURI(frame) {
+    let src = getAttribute(frame, 'src') || '';
+    const [scheme, js] = split(src, ':');
+    if (stringToLowerCase(scheme) === 'javascript') {
+        src = 'javascript:' + applyHookByString(js, 'window', false);
+        setAttribute(frame, 'src', src);
     }
 }
 
-function hookSrcDoc(frames) {
-    for (let i = 0; i < frames.length; i++) {
-        const frame = frames[i];
-        let srcdoc = getAttribute(frame, 'srcdoc');
-        if (srcdoc) {
-            srcdoc = applyHookByString(srcdoc, 'window', true);
-            const html = new Array(srcdoc);
-            handleHTML(html, true);
-            setAttribute(frame, 'srcdoc', html[0]);
-        }
+function hookSrcDoc(frame) {
+    let srcdoc = getAttribute(frame, 'srcdoc');
+    if (srcdoc) {
+        srcdoc = applyHookByString(srcdoc, 'window', true);
+        const html = new Array(srcdoc);
+        handleHTML(html, true);
+        setAttribute(frame, 'srcdoc', html[0]);
     }
 }
 
 function handleHTML(args, callHook) {
     for (let i = 0; i < args.length; i++) {
-        const html = args[i];
         const template = createElement(document, 'template');
-        setInnerHTML(template, html);
+        setInnerHTML(template, args[i]);
         const content = getTemplateContent(template);
         if (!getChildElementCount(content)) {
             continue;
         }
         const frames = getFramesArray(content, false);
-        if (frames.length) {
-            hookOnLoadAttributes(frames);
-            hookJavaScriptURI(frames);
-            hookSrcDoc(frames);
-            args[i] = getInnerHTML(template);
+        for (let j = 0; j < frames.length; j++) {
+            const frame = frames[j];
+            hookOnLoadAttributes(frame);
+            hookJavaScriptURI(frame);
+            hookSrcDoc(frame);
         }
+        args[i] = getInnerHTML(template);
         if (callHook) {
             args[i] = applyHookByString(args[i], 'window', true);
         }

79cfb1fc70974cb0113b10184de86dba2fc2339d
weizman <weizmangal@gmail.com>
2023-01-08 17:53:28 +0200

    make sure complex embed test takes place for object too

--- test/edge.js
+++ test/edge.js
@@ -71,6 +71,30 @@ describe('special cases', () => {
         expect(result).toBe('V,V');
     });
 
+    it('should fail to use atob of an object that was cross origin and then same origin', async () => {
+        if (global.BROWSER === 'SAFARI') {
+            return; // redirecting EMBED by updating src does not work in safari
+        }
+        const result = await browser.executeAsync(function(done) {
+            const bypass = (wins) => done(wins.map(win => (win && win.atob ? win : top).atob('WA==')).join(','));
+            (function(){
+                testdiv1.innerHTML = ('<object id="temp_id_1" type="text/html" data="/">');
+                testdiv2.innerHTML = ('<object id="temp_id_2" type="text/html" data="https://x.com">');
+                setTimeout(() => {
+                    temp_id_2.data = temp_id_1.data;
+                    temp_id_1.data = 'https://x.com';
+                    setTimeout(() => {
+                        temp_id_1.data = temp_id_2.data;
+                        setTimeout(() => {
+                            bypass([window[0], window[1]]);
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+            }());
+        });
+        expect(result).toBe('V,V');
+    });
+
     it('should fail to use atob of an iframe that was reattached to dom', async () => {
         const result = await browser.executeAsync(function(done) {
             const bypass = (wins) => done(wins.map(win => (win && win.atob ? win : top).atob('WA==')).join(','));

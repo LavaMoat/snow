beafd456ca0775d3d0bfdf60613af7b2595a3ea5
weizman <weizmangal@gmail.com>
2023-06-07 15:18:16 +0300

    call findWin on opened windows as well as original top

--- snow.js
+++ snow.js
@@ -130,6 +130,9 @@ const {
   Object,
   getFrameElement
 } = __webpack_require__(14);
+const {
+  forEachOpened
+} = __webpack_require__(134);
 function findWin(win, frameElement) {
   let i = -1;
   while (win[++i]) {
@@ -165,6 +168,16 @@ function findWin(win, frameElement) {
   }
   return null;
 }
+function hookWin(win) {
+  top['SNOW_WINDOW'](win);
+}
+function findAndHookWin(win, frame) {
+  const contentWindow = findWin(win, frame);
+  if (contentWindow) {
+    hookWin(contentWindow);
+  }
+  return !!contentWindow;
+}
 function hook(frames) {
   frames = toArray(frames);
   for (let i = 0; i < frames.length; i++) {
@@ -173,11 +186,8 @@ function hook(frames) {
       continue;
     }
     workaroundChromiumBug(frame);
-    const contentWindow = findWin(top, frame);
-    if (!contentWindow) {
-      continue;
-    }
-    top['SNOW_WINDOW'](contentWindow);
+    findAndHookWin(top, frame);
+    forEachOpened(findAndHookWin, frame);
   }
 }
 module.exports = hook;
@@ -972,6 +982,11 @@ const openeds = new Map();
 function getProxyByOpened(opened) {
   return openeds.get(opened);
 }
+function forEachOpened(cb, arg1) {
+  for (const opened of openeds.keys()) {
+    cb(opened, arg1);
+  }
+}
 function proxy(opened) {
   const target = new Object(null);
   Object.defineProperty(target, 'closed', {
@@ -1018,7 +1033,8 @@ function proxy(opened) {
 }
 module.exports = {
   proxy,
-  getProxyByOpened
+  getProxyByOpened,
+  forEachOpened
 };
 
 /***/ }),
--- src/hook.js
+++ src/hook.js
@@ -2,6 +2,7 @@ const isCrossOrigin = require('is-cross-origin');
 const workaroundChromiumBug = require('./chromium_bug_workaround');
 const {shadows, toArray, getFramesArray, getContentWindowOfFrame, getOwnerWindowOfNode} = require('./utils');
 const {Object, getFrameElement} = require('./natives');
+const {forEachOpened} = require('./proxy');
 
 function findWin(win, frameElement) {
     let i = -1;
@@ -39,6 +40,18 @@ function findWin(win, frameElement) {
     return null;
 }
 
+function hookWin(win) {
+    top['SNOW_WINDOW'](win);
+}
+
+function findAndHookWin(win, frame) {
+    const contentWindow = findWin(win, frame);
+    if (contentWindow) {
+        hookWin(contentWindow);
+    }
+    return !!contentWindow;
+}
+
 function hook(frames) {
     frames = toArray(frames);
     for (let i = 0; i < frames.length; i++) {
@@ -47,11 +60,8 @@ function hook(frames) {
             continue;
         }
         workaroundChromiumBug(frame);
-        const contentWindow = findWin(top, frame);
-        if (!contentWindow) {
-            continue;
-        }
-        top['SNOW_WINDOW'](contentWindow);
+        findAndHookWin(top, frame);
+        forEachOpened(findAndHookWin, frame);
     }
 }
 

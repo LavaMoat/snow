69a7c1054ad72a52c632426e253a1875deee5126
weizman <weizmangal@gmail.com>
2023-06-19 13:41:53 +0300

    fix issue 86

--- src/natives.js
+++ src/natives.js
@@ -112,6 +112,7 @@ function setup(win) {
         getBlobFileType: Object.getOwnPropertyDescriptor(Blob.prototype, 'type').get,
         createObjectURL: Object.getOwnPropertyDescriptor(URL,'createObjectURL').value,
         revokeObjectURL: Object.getOwnPropertyDescriptor(URL,'revokeObjectURL').value,
+        getCommonAncestorContainer: Object.getOwnPropertyDescriptor(Range.prototype, 'commonAncestorContainer').get,
     });
 
     return {
@@ -157,6 +158,7 @@ function setup(win) {
         getBlobFileType,
         createObjectURL,
         revokeObjectURL,
+        getCommonAncestorContainer,
     };
 
     function getContentWindow(element, tag) {
@@ -288,6 +290,10 @@ function setup(win) {
 
     function revokeObjectURL(object) {
         return bag.revokeObjectURL(object);
+    }
+
+    function getCommonAncestorContainer(range) {
+        return bag.getCommonAncestorContainer.call(range);
     }
 }
 
